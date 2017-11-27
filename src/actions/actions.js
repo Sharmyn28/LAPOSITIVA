7f1f7a29a9d30219f67c6bca7c8ab0475d80e51f
unknown <alejandraadriantejda@gmail.com>
2017-11-27 12:29:28 -0500

    firebase subasta

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -85,9 +85,22 @@ export function selectSectionAction(label) {
     });
 }
 
-export function addAution(price, index) {
+export function addAution1(price, index) {
     console.log('new price', price);
     console.log('ruta','tipo/mecanica/'+index+'/precio_subasta')
     database.ref('tipo/mecanica/'+index+'/precio_subasta').set(price);
+}
+
+export function addAution(price, index) {
+    console.log('new price', price);
+    console.log('ruta','tipo/mecanica/'+index+'/precio_subasta')
+    database.ref('tipo/mecanica/'+index+'/precio_subasta').once('value').then(res => {
+        console.log('res-addAution', res.val());
+        const valuePrice = res.val();
+        console.log('suma', parseInt(valuePrice) + price);
+        const suma = parseInt(valuePrice) + price;
+        
+        database.ref('tipo/mecanica/'+index+'/precio_subasta').set(suma)
+    });
 }
 
